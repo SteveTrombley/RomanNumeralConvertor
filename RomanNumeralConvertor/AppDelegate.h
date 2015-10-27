4830ddf33a3b9f398b10ca065583cfe04bd64e91
SteveTrombley <stevetrombley@yahoo.com>
2015-10-27 11:53:23 -0400

    Initial Commit

---
+++ RomanNumeralConvertor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RomanNumeralConvertor
+//
+//  Created by Steve Trombley on 10/27/15.
+//  Copyright © 2015 Steve Trombley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
